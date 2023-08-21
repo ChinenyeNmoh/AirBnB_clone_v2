92811c93e99084eb6db4251bc232afe528d87e16
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-08-21 16:44:05 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from models.city import City
 from os import getenv
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
