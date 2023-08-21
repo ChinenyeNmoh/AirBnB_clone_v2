5b12243eabb8dd9d46b6a5f6d59cdef72aa9ed36
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-08-21 00:43:41 -0700

    partnerA

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-	storage.new(self)
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -57,7 +57,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-	if '_sa_instance_state' in dictionary.keys():
+        if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
 
         return dictionary
