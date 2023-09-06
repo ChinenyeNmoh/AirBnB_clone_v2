fb2c8364133067b7e899dddf6883effdd300c693
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-09-06 12:25:03 -0700

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -21,31 +21,26 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
-    if path.exists(archive_path):
-        put(archive_path, '/tmp/')
-        # Split the archive_path using '/' delimiter to get the file name
-        file_name = archive_path.split("/")[-1]
-        # After getting the file name, split it again to remove the extension
-        no_ext_file = file_name.split(".")[0]
-        release_path = "/data/web_static/releases/"
-        # Create the folder with the file name to keep uncompressed files
-        run('mkdir -p {0}{1}/'.format(release_path, no_ext_file))
-        # Decompress the archived files
-        run('tar -xzf /tmp/{0} -C {1}{2}/'.format
-            (file_name, release_path, no_ext_file)
-            )
-        # Delete the archive from the web server
-        run('rm /tmp/{0}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format
-            (release_path, no_ext_file)
-            )
-        run('rm -rf {0}{1}/web_static'.format(release_path, no_ext_file))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {0}{1}/ /data/web_static/current'.format
-            (release_path, no_ext_file)
-            )
+    """
+    Deploy archive to web server
+    """
+    if path.isfile(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
-    else:
+    except Exception as e:
         return False
 
 
