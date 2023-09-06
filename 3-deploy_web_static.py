ee43cc046060e8659172b67cc48110a624a7b390
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-09-06 12:32:30 -0700

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -47,6 +47,6 @@ def do_deploy(archive_path):
 def deploy():
     """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
-    if archive_path.failed:
+    if archive_path is None:
         return False
     return do_deploy(archive_path)
