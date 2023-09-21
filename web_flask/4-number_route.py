84e2cb96195b160d00c223200b2e413538dfd8c7
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-09-21 06:58:05 -0700

    flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" create a flask application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnh():
+    """ display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """ display C followed by a text given in a text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ display Python followed by a text given in a text variable"""
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def integer(n):
+    """displays number only if integer"""
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
